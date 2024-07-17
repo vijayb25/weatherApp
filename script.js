b7e4a4ba91c068cbf410bf242ab2ccfff56bc8cd
Vijay Baclay <vijay.baclay@proweaver.net>
2024-07-17 13:18:25 +0800

    Js weather app

---
+++ script.js
@@ -0,0 +1,85 @@
+
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apikey = "20b299f8895737b28e711614340b079b";
+
+weatherForm.addEventListener("submit", async event => {
+    event.preventDefault();
+
+    const city = cityInput.value;
+
+    if(city){
+        try {
+            const weatherData = await getWeatherData(city);
+            displayWeatherInfo(weatherData);
+        } catch (error) {
+            console.error(error);
+            displayError(error);
+        }
+    } else{
+        displayError("Please enter a City!");
+    }
+
+});
+
+async function getWeatherData(city){
+
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+
+    const response = await fetch(apiUrl);
+
+    if(!response.ok){
+        throw new Error("Could not fetch weather data!")
+    }
+   
+    return await response.json();
+}
+
+function displayWeatherInfo(data){
+
+    const {name: city, 
+           main: {temp, humidity}, 
+           weather: [{description, id}]} = data;
+
+    card.textContent = "";
+    card.style.display = "flex";
+
+    const cityDisplay = document.createElement("h1");
+    const tempDisplay = document.createElement("p");
+    const humidityDisplay = document.createElement("p");
+    const descDisplay = document.createElement("p");
+
+    cityDisplay.textContent = city;
+    // to get the fahrenheit the formula is (temp - 273.15)* 9/5 + 32
+    tempDisplay.textContent = `${(temp - 273.15).toFixed(1)}°C`;
+    humidityDisplay.textContent = `Humidity: ${humidity}%`;
+    descDisplay.textContent = description.toUpperCase();
+
+
+
+    cityDisplay.classList.add("cityDisplay");
+    tempDisplay.classList.add("tempDisplay");
+    humidityDisplay.classList.add("humidityDisplay");
+    descDisplay.classList.add("descDisplay");
+
+    card.appendChild(cityDisplay);
+    card.appendChild(tempDisplay);
+    card.appendChild(humidityDisplay);
+    card.appendChild(descDisplay);
+}
+
+function getWeatherID(weatherId){
+
+}
+
+function displayError(message){
+
+    const errorDisplay = document.createElement("p");
+    errorDisplay.textContent = message;
+    errorDisplay.classList.add("errorDisplay");
+
+    card.textContent = "";
+    card.style.display = "flex"
+    card.appendChild(errorDisplay);
+}
